d0ed77e90700aaa1083c3dfbb4d031b0a95e9256
suryaprakasamc <suryaprakasam@innoboon.com>
2023-07-19 18:00:21 +0530

    use state with use Effect

--- reactTemplate/src/utils/Counter.tsx
+++ reactTemplate/src/utils/Counter.tsx
@@ -1,6 +1,14 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+
 export const Counter = () => {
   const [count, setCount] = useState(0)
+  const [arr, setArr] = useState(["1", "4"])
+
+  useEffect(() => {
+    console.log("useEffect runs!")
+    console.log(`u clicked ${count} times`)
+  })
+  console.count("component rendered")
   return (
     <>
       <button
@@ -11,6 +19,8 @@ export const Counter = () => {
         Click to Count
       </button>
       <h1>count is {count}</h1>
+      <h3>Arr is {JSON.stringify(arr)}</h3>
+      <input onChange={(e) => setArr([...arr, e.target.value])} />
     </>
   )
 }
